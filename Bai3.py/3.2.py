169954e83c9460c13cfd945bd53ac4bae4758003
nguyenkhoinguyen401-arch <nguyenkhoinguyen401@gmail.com>
2026-05-14 14:20:34 +0700

    Practice

---
+++ Bai3.py/3.2.py
@@ -0,0 +1,159 @@
+# =========================
+# LỚP POINT
+# =========================
+
+import math
+
+
+class Point:
+
+    # Hàm khởi tạo mặc định
+    def __init__(self, x=0, y=1):
+        self.__x = x
+        self.__y = y
+
+    # Nhập tọa độ
+    def read(self):
+        self.__x, self.__y = map(int, input().split())
+
+    # In tọa độ
+    def print(self):
+        print(f"({self.__x}, {self.__y})", end="")
+
+    # Dời điểm
+    def move(self, dx, dy):
+        self.__x += dx
+        self.__y += dy
+
+    # Lấy x
+    def getX(self):
+        return self.__x
+
+    # Lấy y
+    def getY(self):
+        return self.__y
+
+    # Gán x y mới
+    def setXY(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    # Khoảng cách đến gốc tọa độ
+    def distance(self, p=None):
+
+        # Nếu không truyền điểm nào
+        if p is None:
+            return math.sqrt(self.__x ** 2 + self.__y ** 2)
+
+        # Nếu có truyền điểm P
+        return math.sqrt(
+            (self.__x - p.getX()) ** 2 +
+            (self.__y - p.getY()) ** 2
+        )
+
+
+# =========================
+# LỚP COLORPOINT KẾ THỪA POINT
+# =========================
+
+class ColorPoint(Point):
+
+    # Hàm khởi tạo
+    def __init__(self, x=0, y=1, color="xanh"):
+
+        # Gọi constructor của lớp cha
+        super().__init__(x, y)
+
+        self.__color = color
+
+    # Hàm sao chép
+    @classmethod
+    def copy(cls, cp):
+        return cls(cp.getX(), cp.getY(), cp.__color)
+
+    # Nhập dữ liệu
+    def read(self):
+
+        # Nhập cả dòng
+        data = input().split()
+
+        # 2 phần tử đầu là x y
+        x = int(data[0])
+        y = int(data[1])
+
+        # Phần còn lại là màu
+        color = " ".join(data[2:])
+
+        # Gọi setXY của lớp cha
+        self.setXY(x, y)
+
+        self.__color = color
+
+    # In dữ liệu
+    def print(self):
+
+        # Gọi print của lớp cha
+        super().print()
+
+        print(f": {self.__color}")
+
+    # Gán màu mới
+    def setColor(self, color):
+        self.__color = color
+
+
+# =========================
+# LỚP TEST
+# =========================
+
+class C002454:
+
+    # Kịch bản 1
+    @staticmethod
+    def testCase1():
+
+        A = ColorPoint(5, 10, "trắng")
+
+        A.print()
+
+    # Kịch bản 2
+    @staticmethod
+    def testCase2():
+
+        B = ColorPoint()
+
+        B.read()
+
+        B.move(10, 8)
+
+        B.print()
+
+    # Kịch bản 3
+    @staticmethod
+    def testCase3():
+
+        C = ColorPoint(6, 3, "đen")
+
+        D = ColorPoint.copy(C)
+
+        D.print()
+
+        D.setColor("vàng")
+
+        D.print()
+
+        C.print()
+
+    # Main
+    @staticmethod
+    def main():
+
+        C002454.testCase1()
+
+        C002454.testCase2()
+
+        C002454.testCase3()
+
+
+# Chạy chương trình
+C002454.main()
